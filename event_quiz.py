3574eae05d9b131431668ed88958f16896333d76
chaerin-yi <lcr01150@naver.com>
2022-04-08 14:49:23 +0900

    이벤트 퀴즈 코드 수정

--- event_quiz.py
+++ event_quiz.py
@@ -35,7 +35,7 @@ winners = sample(users,4)
 
 print("-- 치킨 당첨자 발표 --")
 print("치킨 당첨자 : {0}".format(winners[0]))
-print("커피 당첨자 : {0}".format(winners[1]))
+print("커피 당첨자 : {0}".format(winners[1:]))
 print("축하합니다.")
 
 
